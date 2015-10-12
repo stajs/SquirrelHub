990c4895165942a52c8cd5b072c34e978f0118f3
stajs <shannonsnz@gmail.com>
2015-10-12 20:33:23 +1300

    Getting there

---
+++ SquirrelHub.Packager/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquirrelHub.Packager
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var releasifyer = new Releasifyer();
+
+			Console.WriteLine("Relevant paths:");
+
+			Console.WriteLine(releasifyer.PackagerDirectory);
+			Console.WriteLine(releasifyer.BuildDirectory);
+			Console.WriteLine(releasifyer.ClientDirectory);
+			Console.WriteLine(releasifyer.ClientBinDirectory);
+			Console.WriteLine(releasifyer.Squirrel);
+			Console.WriteLine(releasifyer.AppIcon);
+			Console.WriteLine();
+
+			releasifyer.Releasify();
+
+			Console.WriteLine("\n\nPress any key to exit.");
+			Console.ReadLine();
+		}
+	}
+}
