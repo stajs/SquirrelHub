3c5414cad267e363332d7002d5bd5649545a616c
stajs <shannonsnz@gmail.com>
2015-10-12 21:24:49 +1300

    v4

--- SquirrelHub.Desktop/Properties/AssemblyInfo.cs
+++ SquirrelHub.Desktop/Properties/AssemblyInfo.cs
@@ -52,6 +52,6 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyFileVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("4.0.0")]
+[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyInformationalVersion("4.0.0")]
